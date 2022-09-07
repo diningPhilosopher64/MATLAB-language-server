1b70bfb8708f5f05d441b1cfc6a957ca57a10dd2
dlilley <dlilley@mathworks.com>
2022-09-07 14:57:16 -0400

    Make CommunicationManager.m methods static

--- matlab/+matlabls/+helpers/CommunicationManager.m
+++ matlab/+matlabls/+helpers/CommunicationManager.m
@@ -1,6 +1,6 @@
 classdef CommunicationManager < handle
-    % Class for facilitating communication between the MATLAB Language Server and the
-    % MATLAB application.
+    % COMMUNICATIONMANAGER Class for facilitating communication between the MATLAB Language
+    % Server and the MATLAB application.
 
     % NOTE: This class serves as a passthrough to Message Service APIs
     % and will be obfuscated to hide their usage from end users.
@@ -9,7 +9,9 @@ classdef CommunicationManager < handle
         function this = CommunicationManager ()
             connector.ensureServiceOn()
         end
+    end
 
+    methods (Static)
         function subscription = subscribe(channel, callback)
             % Subscribe to the given channel. When messages are received on that
             % channel, the provided callback function will be called with the message
