9ce903129bf41a8c6424cd5e98fcce9e4ac07af4
Duncan Lilley <dlilley@mathworks.com>
2022-10-31 09:49:37 -0400

    Fix typo in getCompletionsData help text

--- matlab/+matlabls/+internal/getCompletionsData.m
+++ matlab/+matlabls/+internal/getCompletionsData.m
@@ -1,5 +1,5 @@
 function completionResults = getCompletionsData (code, fileName, cursorPosition)
-    %COMPUTECODEDATA Retrieve completion data for the given code based on the cursor position
+    %GETCOMPLETIONSDATA Retrieve completion data for the given code based on the cursor position
 
     % Force tab completions to load, if necessary
     builtin('_programmingAidsTest', '', '', 0, []);
