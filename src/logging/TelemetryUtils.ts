6544efe87cab6fddda56c592d03da9141b4afcc2
Duncan Lilley <dlilley@mathworks.com>
2023-03-20 15:41:27 -0400

    Add jsdoc

--- src/logging/TelemetryUtils.ts
+++ src/logging/TelemetryUtils.ts
@@ -20,6 +20,12 @@ export enum ActionErrorConditions {
     MatlabUnavailable = 'MATLAB unavailable'
 }
 
+/**
+ * Reports a telemetry event to the client
+ *
+ * @param eventKey The event key
+ * @param data The event's data
+ */
 function reportTelemetry(eventKey: string, data: unknown): void {
     NotificationService.sendNotification(Notification.LogTelemetryData, {
         eventKey,
@@ -27,6 +33,12 @@ function reportTelemetry(eventKey: string, data: unknown): void {
     })
 }
 
+/**
+ * Reports telemetry about a simple action
+ *
+ * @param actionType The action's type
+ * @param data The action's data
+ */
 export function reportTelemetryAction(actionType: string, data = ''): void {
     reportTelemetry(EventKeys.Action, {
         action_type: actionType,
@@ -34,6 +46,13 @@ export function reportTelemetryAction(actionType: string, data = ''): void {
     })
 }
 
+/**
+ * Reports telemetry about a settings change
+ * 
+ * @param settingName The setting's name
+ * @param newValue The new value
+ * @param oldValue The old value
+ */
 export function reportTelemetrySettingsChange(settingName: string, newValue: unknown, oldValue: unknown): void {
     reportTelemetry(EventKeys.SettingChange, {
         setting_name: settingName,
