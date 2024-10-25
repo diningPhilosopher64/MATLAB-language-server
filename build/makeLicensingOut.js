cae09adcbaf84fa3abf9b94b55204f729856340d
dlilley <dlilley@mathworks.com>
2024-10-25 14:33:52 -0400

    Fix server building to be platform independent

---
+++ build/makeLicensingOut.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const path = require('path')
+
+const dirPath = path.join(__dirname, '..', 'out', 'licensing', 'static')
+
+fs.mkdir(dirPath, { recursive: true }, (err) => {
+    if (err) {
+        console.error('Error creating directory: ', err)
+    } else {
+        console.log('Directory created successfully: ', dirPath)
+    }
+})
